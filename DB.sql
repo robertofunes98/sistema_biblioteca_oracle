6afa79c9d4e9b79233862a0319071b8048dea95b
robertofunes98 <robertofunes98@gmail.com>
2019-10-02 10:11:13 -0600

    Contenido:
    - Agregando formulario SDI para mostrar libros
    - Agregando Conexion a DB

--- DB.sql
+++ DB.sql
@@ -6,13 +6,13 @@ create table autor(
 	id_autor int not null auto_increment,
 	nombre varchar(50) not null,
 	primary key pk_autor(id_autor)
-);
+) default charset= utf8;
 
 create table categoria(
 	id_categoria int not null auto_increment,
 	nombre varchar(50) not null,
 	primary key pk_categoria(id_categoria)
-);
+)default charset= utf8;
 
 create table libro(
 	id_libro varchar(10) not null,
@@ -23,4 +23,4 @@ create table libro(
 	primary key pk_libro(id_libro),
 	foreign key fk_libro_autor(id_autor) references autor(id_autor),
 	foreign key fk_libro_categoria(id_categoria) references categoria(id_categoria)
-);
+)default charset= utf8;
---
+++ src/core/ConexionDB.java
@@ -0,0 +1,78 @@
+package core;
+
+import java.sql.*;
+import java.util.LinkedList;
+
+
+
+/**
+ *
+ * @author CATOLICA
+ */
+public class ConexionDB{
+    public Connection conexion=null;
+    //Constructor
+    
+    String db, usuario, clave;
+    
+    public ConexionDB (String dbR, String usuarioR, String claveR) throws ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException {
+        db=dbR;
+        usuario=usuarioR;
+        clave=claveR;
+        
+        //instanciando la conexion.
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+        conexion=DriverManager.getConnection(db,usuario,clave);
+    }
+
+    
+    public DatabaseMetaData getMetaData() throws SQLException
+    {
+        return conexion.getMetaData();
+    }
+    
+    public ResultSet ejecutar(String consulta) throws SQLException
+    {
+        //Objeto tipo statement que maneja la consulta.
+        Statement cons = this.conexion.createStatement();
+        //Ejecutando la consulta.
+        return cons.executeQuery(consulta);
+    }
+    
+    public int ejecutarComando(String comando) throws SQLException {
+        //Objeto tipo Statement que maneja el comando.
+        Statement com = this.conexion.createStatement();
+        //Ejecuta el comando.
+        int cantidadFilas=com.executeUpdate(comando);
+        return cantidadFilas;
+    }
+    
+    public void cerrarConexion() throws SQLException {
+        this.conexion.close();
+    }
+    
+    public LinkedList convertirRsToArrayList(ResultSet rsResultado) throws SQLException
+    {
+        LinkedList<LinkedList> arrayListResultado=new LinkedList<>();
+        
+        ResultSetMetaData rsmd = rsResultado.getMetaData(); 
+
+        int numeroColumnas = rsmd.getColumnCount(); 
+        
+        System.out.println("numero de columnas: "+numeroColumnas);
+        
+        while(rsResultado.next())
+        {
+            LinkedList<String> datos=new LinkedList<>();
+            
+            for(int i=1; i<=numeroColumnas; i++)
+            {
+                datos.add(rsResultado.getString(i));
+            }
+            
+            arrayListResultado.add(datos);
+        }
+        return arrayListResultado;
+            
+    }
+}
